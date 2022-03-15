0ee34954d11bb57b3902d3f488e9f794451e1263
simoneluca00 <si.zaino@libero.it>
2022-03-15 21:56:11 +0100

    Numero random da lista array

--- Gioco dei dadi/asset/js/main.js
+++ Gioco dei dadi/asset/js/main.js
@@ -2,24 +2,24 @@
 let messaggioGiocatore = document.getElementById("numero_giocatore"); 
 
     // 2) definzione tag per stampare numeroComputer
-let messaggioComputer = document.getElementById("numero_computer"); 
+let messaggioComputer = document.getElementById("numero_computer");    
 
-    // 3) definzione btn (click)
+// 3) definzione btn (click)
 let btnGioca = document.getElementById("btnGioca");
-
 
 btnGioca.addEventListener("click",
     function() {
         // 4) Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+        let numeriDado = [1, 2, 3, 4, 5, 6];
         
         // 4.1) numero casuale per il giocatore
-        let numeroGiocatore = Math.floor((Math.random() * 6) + 1 );
+        let numeroGiocatore = Math.floor(Math.random() * numeriDado.length + 1);
         
         // 4.2) numero casuale per il giocatore
-        let numeroComputer = Math.floor((Math.random() * 6) + 1 );
+        let numeroComputer = Math.floor(Math.random() * numeriDado.length + 1);
         
         // 5) Stampare sulla pagina i numeri random per il giocatore e per il computer
-
         messaggioGiocatore.innerHTML = `${numeroGiocatore}`;
         messaggioComputer.innerHTML = `${numeroComputer}`;
         
