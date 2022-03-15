4e68ca605777b39f09bd14588a920d12ac2f8dd7
simoneluca00 <si.zaino@libero.it>
2022-03-15 19:07:52 +0100

    Gioco dei dadi completato

---
+++ Gioco dei dadi/asset/js/main.js
@@ -0,0 +1,36 @@
+    // 1) definzione tag per stampare numeroGiocatore
+let messaggioGiocatore = document.getElementById("numero_giocatore"); 
+
+    // 2) definzione tag per stampare numeroComputer
+let messaggioComputer = document.getElementById("numero_computer"); 
+
+    // 3) definzione btn (click)
+let btnGioca = document.getElementById("btnGioca");
+
+
+btnGioca.addEventListener("click",
+    function() {
+        // 4) Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+        
+        // 4.1) numero casuale per il giocatore
+        let numeroGiocatore = Math.floor((Math.random() * 6) + 1 );
+        
+        // 4.2) numero casuale per il giocatore
+        let numeroComputer = Math.floor((Math.random() * 6) + 1 );
+        
+        // 5) Stampare sulla pagina i numeri random per il giocatore e per il computer
+
+        messaggioGiocatore.innerHTML = `${numeroGiocatore}`;
+        messaggioComputer.innerHTML = `${numeroComputer}`;
+        
+        // 6) Stabilire il vincitore, in base a chi fa il punteggio più alto.
+        if(numeroGiocatore > numeroComputer) {
+            document.getElementById("result").innerHTML = `Complimenti... fai subito capire al computer chi comanda!`;
+        } else if (numeroGiocatore < numeroComputer) {
+            document.getElementById("result").innerHTML = `Peccato... il computer è riuscito a debuggarti!`;
+        } else if (numeroComputer == numeroGiocatore) {
+            document.getElementById("result").innerHTML = `Nessuno spargimento di sangue... chi è il fortunato tra i due?`
+        }
+    }
+)
+
